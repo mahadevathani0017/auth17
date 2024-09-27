65f88e5d47f122897e51f52d33603fd41634d5ab
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-09-27 15:26:14 +0530

    start to learn authentication

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
+import {app} from "./firebase";
+
+
+const auth=getAuth(app);
+
+function App() {
+
+  const signupUser = () => {
+    createUserWithEmailAndPassword(
+      auth,
+      "mahadev@123.gmail",
+      "mahadev@123"
+    ).then((value)=> console.log(value))
+  };
+     
+  
+   return (
+    <>
+    <h1>Firebase start</h1>
+    <button onClick={signupUser}>ClickOn</button>
+    </>
+   )
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_ag4vwjFK-XJpMj_h_X5mAhLRRv25zUA",
+  authDomain: "app-6bc40.firebaseapp.com",
+  projectId: "app-6bc40",
+  storageBucket: "app-6bc40.appspot.com",
+  messagingSenderId: "970355323303",
+  appId: "1:970355323303:web:3d25269c4a3015a0058682",
+  databaseURL: "https://app-6bc40-default-rtdb.firebaseio.com",
+};
+
+export const app =initializeApp(firebaseConfig);
